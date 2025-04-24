32a91bfe7c4ec5087b7774be5af80fb776e76623
tahomee <thaothanh6g@gmail.com>
2025-04-24 12:50:24 +0700

    fix pixel

--- lib/widgets/trending_place.dart
+++ lib/widgets/trending_place.dart
@@ -37,104 +37,108 @@ class _TrendingPlaceState extends State<TrendingPlace> {
           width: MediaQuery.of(context).size.width,
           child: Card(
             shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10.0)),
+              borderRadius: BorderRadius.circular(10.0),
+            ),
             elevation: 3.0,
-            child: Column(
-              children: <Widget>[
-                Stack(
-                  children: <Widget>[
-                    SizedBox(
-                      height: MediaQuery.of(context).size.height / 3.5,
-                      width: MediaQuery.of(context).size.width,
-                      child: ClipRRect(
-                        borderRadius: const BorderRadius.only(
-                          topLeft: Radius.circular(10),
-                          topRight: Radius.circular(10),
-                        ),
-                        child: Image.network(
-                          widget.place.img,
-                          fit: BoxFit.cover,
-                        ),
-                      ),
-                    ),
-                    Positioned(
-                      top: 6.0,
-                      right: 6.0,
-                      child: Card(
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(4.0)),
-                        child: Padding(
-                          padding: const EdgeInsets.all(2.0),
-                          child: Row(
-                            children: <Widget>[
-                              Icon(
-                                Icons.star,
-                                color: Constants.ratingBG,
-                                size: 10.0,
-                              ),
-                              Text(
-                                " ${widget.place.rating} ",
-                                style: const TextStyle(
-                                  fontSize: 10.0,
-                                ),
-                              ),
-                            ],
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Stack(
+                    children: <Widget>[
+                      SizedBox(
+                        height: MediaQuery.of(context).size.height / 3.5,
+                        width: MediaQuery.of(context).size.width,
+                        child: ClipRRect(
+                          borderRadius: const BorderRadius.only(
+                            topLeft: Radius.circular(10),
+                            topRight: Radius.circular(10),
+                          ),
+                          child: Image.network(
+                            widget.place.img,
+                            fit: BoxFit.cover,
                           ),
                         ),
                       ),
-                    ),
-                    Positioned(
-                      top: 6.0,
-                      left: 6.0,
-                      child: Card(
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(3.0)),
-                        child: Padding(
-                          padding: const EdgeInsets.all(4.0),
-                          child: Text(
-                            " ${widget.place.openTime.toString()}-${widget.place.closeTime.toString()}",
-                            style: const TextStyle(
-                              fontSize: 10.0,
-                              color: Colors.green,
-                              fontWeight: FontWeight.bold,
+                      Positioned(
+                        top: 6.0,
+                        right: 6.0,
+                        child: Card(
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(4.0)),
+                          child: Padding(
+                            padding: const EdgeInsets.all(2.0),
+                            child: Row(
+                              children: <Widget>[
+                                Icon(
+                                  Icons.star,
+                                  color: Constants.ratingBG,
+                                  size: 10.0,
+                                ),
+                                Text(
+                                  " ${widget.place.rating} ",
+                                  style: const TextStyle(
+                                    fontSize: 10.0,
+                                  ),
+                                ),
+                              ],
                             ),
                           ),
                         ),
                       ),
-                    ),
-                  ],
-                ),
-                const SizedBox(height: 7.0),
-                Padding(
-                  padding: const EdgeInsets.only(left: 15.0),
-                  child: SizedBox(
-                    width: MediaQuery.of(context).size.width,
-                    child: Text(
-                      widget.place.name,
-                      style: const TextStyle(
-                        fontSize: 20.0,
-                        fontWeight: FontWeight.w800,
+                      Positioned(
+                        top: 6.0,
+                        left: 6.0,
+                        child: Card(
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(3.0)),
+                          child: Padding(
+                            padding: const EdgeInsets.all(4.0),
+                            child: Text(
+                              " ${widget.place.openTime}-${widget.place.closeTime}",
+                              style: const TextStyle(
+                                fontSize: 10.0,
+                                color: Colors.green,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ),
                       ),
-                      textAlign: TextAlign.left,
+                    ],
+                  ),
+                  const SizedBox(height: 7.0),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 15.0),
+                    child: SizedBox(
+                      width: MediaQuery.of(context).size.width,
+                      child: Text(
+                        widget.place.name,
+                        style: const TextStyle(
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.w800,
+                        ),
+                        textAlign: TextAlign.left,
+                      ),
                     ),
                   ),
-                ),
-                const SizedBox(height: 7.0),
-                Padding(
-                  padding: const EdgeInsets.only(left: 15.0),
-                  child: SizedBox(
-                    width: MediaQuery.of(context).size.width,
-                    child: Text(
-                      widget.place.address,
-                      style: const TextStyle(
-                        fontSize: 12.0,
-                        fontWeight: FontWeight.w300,
+                  const SizedBox(height: 7.0),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 15.0),
+                    child: SizedBox(
+                      width: MediaQuery.of(context).size.width,
+                      child: Text(
+                        widget.place.address,
+                        style: const TextStyle(
+                          fontSize: 12.0,
+                          fontWeight: FontWeight.w300,
+                        ),
                       ),
                     ),
                   ),
-                ),
-                const SizedBox(height: 10.0),
-              ],
+                  const SizedBox(height: 10.0),
+                ],
+              ),
             ),
           ),
         ),
